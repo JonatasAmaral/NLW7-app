833d41adc7e1bfb82a975c5c7d5e3e27a58c7ff3
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-04 23:12:33 -0300

    add message list

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+import { Message } from "../Message";
+
+import { styles } from "./styles";
+
+export function MessageList() {
+	const messagesList = [...Array(3)];
+
+	return (
+		<ScrollView
+			style={styles.container}
+			contentContainerStyle={styles.content}
+			keyboardShouldPersistTaps="never"
+		>
+			{messagesList.map((_, j) => {
+				const randNum = Math.floor(Math.random() * 99);
+				const message = {
+					id: `${randNum}`,
+					text: "um texto qualquer",
+					user: {
+						name: "O Nome",
+						avatar_url: `https://randomuser.me/api/portraits/thumb/men/${randNum}.jpg`,
+					},
+				};
+
+				return <Message key={message.id} data={message} />;
+			})}
+		</ScrollView>
+	);
+}
---
+++ src/components/MessageList/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexBasis: 0,
+		paddingHorizontal: 20,
+	},
+	content: {
+		paddingTop: 75,
+		paddingBottom: 25,
+	},
+});

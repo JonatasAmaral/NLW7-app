f0d6a9792e7f1af24247fe8de79d6bc48caa0459
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-07 16:23:19 -0300

    store auth data and recover it on new session

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as AuthSessions from "expo-auth-session";
 
 import env from "../../.env";
@@ -6,6 +7,8 @@ import { api } from "../services/api";
 
 const CLIENT_ID = env.GITHUB_CLIENT_ID;
 const SCOPE = "read:user";
+const USER_STORAGE = "@nlw7-app:user";
+const TOKEN_STORAGE = "@nlw7-app:token";
 
 type User = {
 	id: string;
@@ -46,9 +49,8 @@ function AuthProvider({ children }: AuthProviderProps) {
 
 	// TODO: corrigir na versão web. Nova janela não redireciona para github
 	async function signIn() {
+		setIsSigningIn(true);
 		try {
-			setIsSigningIn(true);
-
 			const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=${SCOPE}`;
 			const authSessionResponse = (await AuthSessions.startAsync({
 				authUrl,
@@ -61,7 +63,11 @@ function AuthProvider({ children }: AuthProviderProps) {
 				const authResponse = await api.post<AuthResponse>("/authenticate", {
 					code: authSessionResponse.params.code,
 				});
-				const { user: userResponse, token } = authResponse.data as AuthResponse;
+				const { user: userResponse, token } = authResponse.data;
+
+				api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+				await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(userResponse));
+				await AsyncStorage.setItem(TOKEN_STORAGE, JSON.stringify(token));
 
 				setUser(userResponse);
 			}
@@ -74,6 +80,24 @@ function AuthProvider({ children }: AuthProviderProps) {
 	async function signOut() {
 		// signOut function
 	}
+
+	useEffect(() => {
+		setIsSigningIn(true);
+		async function loadStorageData() {
+			const userStorage = await AsyncStorage.getItem(USER_STORAGE);
+			const tokenStorage = await AsyncStorage.getItem(TOKEN_STORAGE);
+
+			if (userStorage && tokenStorage) {
+				api.defaults.headers.common["Authorization"] = `Bearer ${tokenStorage}`;
+				setUser(JSON.parse(userStorage));
+			}
+
+			setIsSigningIn(false);
+		}
+
+		loadStorageData();
+	}, []);
+
 	return (
 		<AuthContext.Provider value={{ user, isSigningIn, signIn, signOut }}>
 			{children}

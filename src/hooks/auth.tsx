522144b136bc6238b08e84bfe018a00d944636bf
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-07 16:06:21 -0300

    ask github to authenticate user

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,11 @@
 import React, { createContext, useContext, useState } from "react";
+import * as AuthSessions from "expo-auth-session";
+
+import env from "../../.env";
+import { api } from "../services/api";
+
+const CLIENT_ID = env.GITHUB_CLIENT_ID;
+const SCOPE = "read:user";
 
 type User = {
 	id: string;
@@ -18,14 +25,51 @@ type AuthProviderProps = {
 	children: React.ReactNode;
 };
 
+type AuthResponse = {
+	token: string;
+	user: User;
+};
+
+type AuthorizationResponse = {
+	params: {
+		code?: string;
+		error?: string;
+	};
+	type?: string;
+};
+
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-	const [user] = useState<User | null>(null);
-	const [isSigningIn] = useState<boolean>(false);
+	const [user, setUser] = useState<User | null>(null);
+	const [isSigningIn, setIsSigningIn] = useState<boolean>(false);
 
+	// TODO: corrigir na versão web. Nova janela não redireciona para github
 	async function signIn() {
-		// signIn function
+		try {
+			setIsSigningIn(true);
+
+			const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=${SCOPE}`;
+			const authSessionResponse = (await AuthSessions.startAsync({
+				authUrl,
+			})) as AuthorizationResponse;
+
+			if (
+				authSessionResponse.type === "success" &&
+				authSessionResponse.params.error !== "access_denied"
+			) {
+				const authResponse = await api.post<AuthResponse>("/authenticate", {
+					code: authSessionResponse.params.code,
+				});
+				const { user: userResponse, token } = authResponse.data as AuthResponse;
+
+				setUser(userResponse);
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsSigningIn(false);
+		}
 	}
 	async function signOut() {
 		// signOut function

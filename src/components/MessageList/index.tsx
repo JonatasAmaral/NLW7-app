876c4f5dfc0f1497c2201bb80536d97cc3f26452
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-08 00:23:23 -0300

    add websocket fetch of new messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
+import { io } from "socket.io-client";
 
 import { api } from "../../services/api";
 import { Message, MessageProps } from "../Message";
 
 import { styles } from "./styles";
+
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on("new_message", (message: MessageProps) => {
+	messagesQueue.push(message);
+});
 
 export function MessageList() {
 	const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
@@ -16,6 +24,20 @@ export function MessageList() {
 		}
 		fetchMessages();
 	}, []);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (messagesQueue.length > 0) {
+				setCurrentMessages((prev) => [
+					messagesQueue.shift() as MessageProps,
+					prev[0],
+					prev[1],
+				]);
+			}
+		}, 2000);
+
+		return () => clearInterval(timer);
+	}, [messagesQueue]);
 
 	return (
 		<ScrollView
